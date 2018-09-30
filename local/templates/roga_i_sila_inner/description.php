35a3228e9af7ebb10d723d28f73c5d24fc24bbc2
Damir <blood1area@gmail.com>
2018-09-30 14:53:39 +0300

    first comm

---
+++ local/templates/roga_i_sila_inner/description.php
@@ -0,0 +1,14 @@
+<?
+$arTemplate = array (
+  'NAME' => 'roga_i_sila_inner',
+  'DESCRIPTION' => '',
+  'SORT' => 1,
+  'TYPE' => '',
+  'EDITOR_STYLES' => 
+  array (
+    0 => 'local/templates/roga_i_sila_inner/styles.css',
+    1 => 'local/templates/roga_i_sila_inner/template_styles.css',
+    2 => 'local/templates/roga_i_sila_inner/css/base.css',
+  ),
+);
+?>
